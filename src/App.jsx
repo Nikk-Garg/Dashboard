9f5e66a99c46e252897024de2203dc0895785dda
Nitika Aggarwal <aggarwalnitika189@gmail.com>
2026-07-12 20:13:08 +0530

    Added dashboard components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './App.css'
 import Sidebar from './components/Sidebar/Sidebar'
+import MainDash from './components/MainDash.jsx/MainDash'
 
 const App = () => {
   return (
     <div className='App'>
       <div className='AppGlass'>
         <Sidebar/>
-        <div></div>
+        <MainDash/>
         <div></div>
       </div>
     </div>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import Logo from '../../imgs/logo.png'
 import "./Sidebar.css"
-import { SidebarData } from '../../Data/Data'
-
+import { SidebarData } from '../../Data/Data.js'
+import {UilSignOutAlt} from '@iconscout/react-unicons'
 
 
 const Sidebar = () => {
+
+    const [ selected, setSelected] = useState(0)
+
+
   return (
     <div className="Sidebar">
         {/* logo */}
@@ -18,14 +22,19 @@ const Sidebar = () => {
         <div className="menu">
           {SidebarData.map((item, index)=>{
             return(
-                <div className="menuItem">
+                <div className={selected === index ? 'menuItem active': 'menuItem'}
+                    onClick={()=> setSelected(index)}
+                 key={index}>
                     <item.icon/>
                     <span>
-                        <item.heading/>
+                        {item.heading}
                     </span>
                 </div>
             )
           })}
+
+          <div className="menuItem">
+            <UilSignOutAlt/> </div>
         </div>
     </div>
   )
